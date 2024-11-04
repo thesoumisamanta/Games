3b0b4d0568c9656c2e50e8bcfea975f9cec8d75a
thesoumisamanta <samantasoumi10@gmail.com>
2024-11-03 21:51:43 -0500

    choose the random number which is chosen by the opponent

--- GuessTheNumber.c
+++ GuessTheNumber.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
+    
+    srand(time(0));
+    int randomNumber = (rand() % 100);
+    int noOfGuesses = 0;
+    int guessed;
+
+    
+    
+    do
+    {
+        printf("Guess the number : ");
+        scanf("%d", &guessed);
+
+        if(guessed > randomNumber){
+            printf("Guessed lower number\n");
+        } else if (guessed < randomNumber){
+            printf("Guessed higher number\n");
+        }
+
+        noOfGuesses++;
+
+    } while (guessed != randomNumber);
+    
+    printf("You guessed the number in %d guesses", noOfGuesses);
     
     return 0;
 }
